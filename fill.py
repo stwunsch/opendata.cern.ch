5684369cb551bd619d2a5cfe1e3787e20e47ae73
Stefan Wunsch <stefan.wunsch@cern.ch>
2019-08-13 08:40:46 +0200

    [REMOVE ME] Add helper scripts (only temporary)

---
+++ fill.py
@@ -0,0 +1,69 @@
+import json
+import copy
+
+path = "cernopendata/modules/fixtures/data/records/cms-derived-nanoaod-outreach-higgstautau.json"
+
+d = json.load(open(path, "r"))
+ref = d[0]
+if ref["recid"] != "12351":
+    raise Exception
+
+links = [
+        6004,
+        6004,
+        6004,
+        6004,
+        6004,
+        6004,
+        6004,
+        6004,
+        6004,
+        ]
+titles = [
+    "GluGluToHToTauTau",
+    "VBF_HToTauTau",
+    "DYJetsToLL",
+    "TTbar",
+    "W1JetsToLNu",
+    "W2JetsToLNu",
+    "W3JetsToLNu",
+    "Run2012B_TauPlusX",
+    "Run2012C_TauPlusX"
+        ]
+events = [
+        476963,
+        491653,
+        30458871,
+        6423106,
+        29784800,
+        30693853,
+        15241144,
+        35647508,
+        51303171
+        ]
+sizes = [
+        220877894,
+        263790659,
+        10516614885,
+        4139529585,
+        11396739482,
+        13194361062,
+        7205227863,
+        13367993207,
+        19403533123
+        ]
+
+n = []
+for i in range(len(titles)):
+    tmp = copy.deepcopy(ref)
+    tmp["validation"]["links"][0]["recid"] = str(links[i])
+    tmp["recid"] = str(int(12351+i))
+    tmp["title"] = ref["title"].replace("GluGluToHToTauTau", titles[i])
+    tmp["files"][0]["uri"] = "root://some/folder/" + titles[i] + ".root"
+    tmp["files"][0]["checksum"] = "adler32:51f0616" + str(i)
+    tmp["files"][0]["size"] = sizes[i]
+    tmp["distribution"]["size"] = sizes[i]
+    tmp["distribution"]["number_events"] = events[i]
+    n.append(tmp)
+
+json.dump(n, open(path, "w"), sort_keys=True, indent=4, separators=(',', ': '))
