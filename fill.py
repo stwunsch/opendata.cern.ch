8c02341e832017956c0e90380c88a9b0a92b8604
Stefan Wunsch <stefan.wunsch@cern.ch>
2019-08-14 14:43:16 +0200

    x

---
+++ fill.py
@@ -0,0 +1,81 @@
+import json
+import copy
+
+path = "cernopendata/modules/fixtures/data/records/cms-derived-nanoaod-outreach-higgstautau.json"
+
+d = json.load(open(path, "r"))
+ref = d[0]
+if ref["recid"] != "12351":
+    raise Exception
+
+links = [
+        8001,
+        9743,
+        7730,
+        9518,
+        9863,
+        9864,
+        9865,
+        6024,
+        6050,
+        ]
+titles = [
+    "GluGluToHToTauTau",
+    "VBF_HToTauTau",
+    "DYJetsToLL",
+    "TTbar",
+    "W1JetsToLNu",
+    "W2JetsToLNu",
+    "W3JetsToLNu",
+    "Run2012B_TauPlusX",
+    "Run2012C_TauPlusX"
+        ]
+events = [
+        476963,
+        491653,
+        30458871,
+        6423106,
+        29784800,
+        30693853,
+        15241144,
+        35647508,
+        51303171
+        ]
+sizes = [
+        196517149,
+        231849713,
+        9247252629,
+        3528995041,
+        10731401995,
+        12161887620,
+        6544850738,
+        10918632568,
+        15886107547
+        ]
+hashes = [
+        "2b025925",
+        "8967f330",
+        "dfd0d57c",
+        "99ffd168",
+        "e5d19fd7",
+        "26b03d60",
+        "5a2b198a",
+        "8b8629d5",
+        "5590ee9f"
+        ]
+
+n = []
+for i in range(len(titles)):
+    tmp = copy.deepcopy(ref)
+    tmp["validation"]["links"][0]["recid"] = str(links[i])
+    tmp["recid"] = str(int(12351+i))
+    tmp["title"] = ref["title"].replace("GluGluToHToTauTau", titles[i])
+    tmp["files"][0]["uri"] = "root://eospublic.cern.ch//eos/opendata/cms/derived-data/AOD2NanoAODOutreachTool/" + titles[i] + ".root"
+    tmp["files"][0]["checksum"] = "adler32:" + hashes[i]
+    tmp["files"][0]["size"] = sizes[i]
+    tmp["distribution"]["size"] = sizes[i]
+    tmp["distribution"]["number_events"] = events[i]
+    tmp["relations"][0]["recid"] = str(links[i])
+    n.append(tmp)
+
+json.dump(n, open(path, "w"), sort_keys=True, indent=4, separators=(',', ': '))
---
+++ insert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker exec -i -t opendatacernch_web_1 cernopendata fixtures records --mode insert-or-replace \
+    -f /code/cernopendata/modules/fixtures/data/records/cms-tools-nanoaod-outreach-dimuon-spectrum.json \
+    -f /code/cernopendata/modules/fixtures/data/records/cms-tools-nanoaod-outreach-higgstautau.json \
+    -f /code/cernopendata/modules/fixtures/data/records/cms-derived-nanoaod-outreach-higgstautau.json \
+    -f /code/cernopendata/modules/fixtures/data/records/cms-derived-nanoaod-outreach-DoubleMuParked.json
